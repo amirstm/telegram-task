f22db455dfa3c6a641313f8cd4528fd83a93a727
AmirSTM <amirstm@gmail.com>
2023-09-30 13:59:56 +0330

    Organize cron jobs on president

--- telegram_task/president.py
+++ telegram_task/president.py
@@ -8,7 +8,8 @@ from __future__ import annotations
 import logging
 import asyncio
 import threading
-import time
+from time import sleep
+from datetime import datetime, date
 import telegram
 import telegram.ext
 import telegram_task.line
@@ -29,7 +30,7 @@ class President:
         self.telegram_app = telegram_app
         self.telegram_admin_id = telegram_admin_id
         self.__telegram_que: asyncio.Queue = None
-        self.__is_listening: bool = False
+        self.__is_running: bool = False
         self._lines: list[telegram_task.line.LineManager] = []
         self.__operation_loop: asyncio.AbstractEventLoop = None
         self.__killer_thread: threading.Thread = None
@@ -41,7 +42,7 @@ class President:
     def start_operation(self, lifespan: int = 0) -> None:
         """Start the operation of the enterprise after full initiation"""
         self._LOGGER.info("President is starting the operation.")
-        self.__is_listening = True
+        self.__is_running = True
         self.__operation_loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.__operation_loop)
         if lifespan > 0:
@@ -65,12 +66,12 @@ class President:
         self._LOGGER.info(
             "President is starting the operation asynchronously."
         )
-        self.__is_listening = True
+        self.__is_running = True
         try:
             group = self.__operation_group()
             await asyncio.wait_for(group, timeout=lifespan)
         except asyncio.exceptions.TimeoutError:
-            self.__is_listening = False
+            self.__is_running = False
             self._LOGGER.info("Telegram bot listener is terminated.")
         except Exception as ex:
             self._LOGGER.error(ex, exc_info=True)
@@ -84,14 +85,14 @@ class President:
             "Automatic killer is set to stop operation after %d seconds.",
             lifespan
         )
-        time.sleep(lifespan)
+        sleep(lifespan)
         self._LOGGER.info("Automatic killer is killing the operation.")
         self.stop_operation()
 
     def stop_operation(self) -> None:
         """Stop the enterprise operation"""
         self._LOGGER.info("President is stopping the operation.")
-        self.__is_listening = False
+        self.__is_running = False
         self.__operation_loop.stop()
 
     async def __init_updater(self) -> None:
@@ -112,11 +113,34 @@ class President:
             self._LOGGER.info("Terminating telegram bot listener.")
 
     async def __telegram_listener(self) -> None:
+        """Waiting for updates from telegram"""
         self._LOGGER.info("telegram_listener loop has started.")
-        while self.__is_listening:
+        while self.__is_running:
             new_update = await self.__telegram_que.get()
             self._LOGGER.info("Update from telegram %s", new_update)
         self._LOGGER.info("telegram_listener is done.")
+
+    async def __handle_crons(self) -> None:
+        """Handling cron jobs associated with lines"""
+        self._LOGGER.info("Handling cron jobs has started.")
+        while self.__is_running:
+            daily_tasks = self.get_daily_tasks()
+            self._LOGGER.info(
+                "Handling [%d] cron jobs for [%s]",
+                len(daily_tasks),
+                date.today()
+            )
+
+    def get_daily_tasks(self):
+        """Get cron task for the rest of the day"""
+        now_datetime = datetime.now()
+        now_time = now_datetime.time()
+        weekday = now_datetime.weekday()
+        return [
+            [x, y] for x in self._lines
+            for y in x.cron_job_orders
+            if weekday not in y.off_days and y.daily_run_time > now_time
+        ]
 
     def add_line(self, *args: telegram_task.line.LineManager) -> None:
         """Add new line managers to the enterprise"""
