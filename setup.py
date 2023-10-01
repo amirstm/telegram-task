715e960a390cb81d5fefd34851c47294336ec56e
AmirSTM <amirstm@gmail.com>
2023-10-01 13:01:35 +0330

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ setuptools.setup(
     author="Arka Equities & Securities",
     author_email="info@arkaequities.com",
     description="A telegram bot task manager wrapper.",
-    # long_description="Utilities for data mining and auto-trading in Tehran Stock Exhchange. \
-    #     Contains TSETMC API implementation and other useful interfaces for securities market \
-    #     data catching and processing.",
+    long_description="""
+This project can be used as an interface for a simple task manager.
+A telegram bot token and the ID of the manager account are the only \
+essential input variables needed to enable this project as your \
+personal assistant in running and handling jobs on a daily basis.
+    """,
     packages=setuptools.find_packages(),
     install_requires=["python-telegram-bot[socks]", "python-dotenv"],
     classifiers=[
