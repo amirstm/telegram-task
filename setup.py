74f900a07a01d10da8024982e8854f523168d8f5
AmirSTM <amirstm@gmail.com>
2023-10-08 12:45:11 +0330

    Update dependent packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="telegram_task",
-    version="1.0.1",
+    version="1.0.2",
     author="Arka Equities & Securities",
     author_email="info@arkaequities.com",
     description="A telegram bot task manager wrapper.",
@@ -14,7 +14,11 @@ essential input variables needed to enable this project as your \
 personal assistant in running and handling jobs on a daily basis.
     """,
     packages=setuptools.find_packages(),
-    install_requires=["python-telegram-bot[socks]", "python-dotenv"],
+    install_requires=[
+	"python-telegram-bot[socks,job-queue]==20.6", 
+	"python-dotenv==1.0.0",
+	"pytz==2023.3.post1"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: POSIX :: Linux",
